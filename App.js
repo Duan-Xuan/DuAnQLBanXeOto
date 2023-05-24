6f227534e129577c7c321753c2c9a6483c6c73b3
Duan-Xuan <duannxph21488@fpt.edu.vn>
2023-05-25 00:34:46 +0700

    Update tí :)

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Chào mừng đến với chúng tôi!</Text>
       <StatusBar style="auto" />
     </View>
   );
