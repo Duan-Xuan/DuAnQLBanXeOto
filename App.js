3fb2b1b154af32b5fa4d8ea1f3de72559ff188d8
lamquang411 <lamquang4112003@gmail.com>
2023-05-25 20:38:14 +0700

    quang

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={Home} />
-        {/* quang */}
+        {/* quang12 */}
       </Stack.Navigator>
     </NavigationContainer>
   );
