143c4903c58cef6c6fbc7428ee5d53fbfa4c2945
Duan-Xuan <duannxph21488@fpt.edu.vn>
2023-05-27 12:46:01 +0700

    Duẫn hoàn thành các chức năng của hãng

--- ClassFunction/TtHang.js
+++ ClassFunction/TtHang.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet, TextInput, Alert, View, TouchableOpacity, ImageBackground, FlatList } from 'react-native'
+import { Text, StyleSheet, TextInput, Alert, View, TouchableOpacity, ImageBackground, Modal } from 'react-native'
 import React, { useState } from 'react'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import background from '../assets/backgroud.png'
@@ -11,9 +11,41 @@ const TtHang = (props) => {
     const { id } = props.route.params
 
     const [object, setobject] = useState([])
+    const [isModal, setIsModal] = useState(false)
+    const [name, setName] = useState('')
+
+    const modal = () => {
+        setIsModal(!isModal)
+    }
 
     const previous = () => {
         navigation.navigate('Hang')
+    }
+
+    const update = () => {
+        if (name == '') {
+            Alert.alert('Thông báo', 'Vui lòng nhập đủ thông tin')
+            return
+        }
+        let obj = { name: name };
+        fetch(api_url + id, {
+            method: 'PUT',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(obj)
+        })
+            .then((res) => {
+                if (res.status == 200) {
+                    Alert.alert('Thông báo', 'Sửa thông tin thành công!')
+                    getList()
+                    modal()
+                }
+            })
+            .catch((ex) => {
+                console.log(ex);
+            });
     }
 
     const xoa = () => {
@@ -69,7 +101,7 @@ const TtHang = (props) => {
                     <Icon name="reply" size={45} color="white" />
                 </TouchableOpacity>
                 <Text style={styles.title}>Thông Tin Hãng</Text>
-                <TouchableOpacity style={styles.button1} >
+                <TouchableOpacity onPress={modal} style={styles.button1} >
                     <Icon name="paint-brush" size={45} color="white" />
                 </TouchableOpacity>
             </View>
@@ -80,6 +112,30 @@ const TtHang = (props) => {
                     <Icon name="trash-o" size={45} color="red" />
                 </TouchableOpacity>
             </View>
+            <Modal animationType="slide"
+                transparent={true}
+                visible={isModal}
+                onRequestClose={() => { modal }}>
+                <View style={styles.khungngoai}>
+                    <View style={styles.khungtrong}>
+                        <Text style={styles.title2}>Update Hãng</Text>
+                        <Text style={styles.text3}>Tên Hãng</Text>
+                        <TextInput children={object.name} style={styles.textInputNgoai} onChangeText={(content) => { setName(content) }} placeholder='Nhập hãng' />
+                        <View style={{ flexDirection: 'row', marginTop: '5%' }}>
+                            <TouchableOpacity onPress={modal} style={styles.button3}>
+                                <Text style={{ fontWeight: 'bold', color: 'white' }}>
+                                    Đóng
+                                </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={update} style={styles.button3}>
+                                <Text style={{ fontWeight: 'bold', color: 'white' }}>
+                                    Lưu
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
         </ImageBackground>
     )
 }
@@ -98,6 +154,11 @@ const styles = StyleSheet.create({
         fontSize: 30,
         fontWeight: 'bold',
         color: 'white',
+    },
+    title2: {
+        fontSize: 30,
+        marginTop: '5%',
+        fontWeight: 'bold'
     },
     box1: {
         marginTop: '25%',
@@ -124,11 +185,48 @@ const styles = StyleSheet.create({
         fontSize: 25,
         color: 'red'
     },
+    text3: {
+        marginTop: '5%',
+        fontWeight: 'bold',
+    },
     button1: {
         marginLeft: '5%',
         marginRight: '5%',
     },
     button2: {
         marginTop: '15%'
+    },
+    khungngoai: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    khungtrong: {
+        height: 300,
+        borderRadius: 20,
+        backgroundColor: 'white',
+        marginLeft: 40,
+        marginRight: 40,
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: 'green',
+    },
+    textInputNgoai: {
+        width: 250,
+        marginTop: '5%',
+        marginBottom: '7%',
+        borderWidth: 1,
+        borderRadius: 10,
+        padding: 10,
+        color: 'red',
+        borderColor: 'green',
+    },
+    button3: {
+        width: 100,
+        height: 50,
+        margin: '5%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        backgroundColor: 'green'
     },
 })
